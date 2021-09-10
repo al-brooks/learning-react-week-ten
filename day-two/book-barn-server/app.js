257c51d8c63167d438048ecd16da936d74555dfe
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-09-10 16:49:29 -0400

    added database to server

--- day-two/book-barn-server/app.js
+++ day-two/book-barn-server/app.js
@@ -1,65 +1,27 @@
-const express = require('express');
+require('dotenv').config();
+global.express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 8080;
+global.fetch = require('node-fetch');
+global.session = require('express-session');
+const pgp = require('pg-promise')();
+const connectionString = process.env.DB_CONNECT;
+global.db = pgp(connectionString);
+global.bcrypt = require('bcrypt');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const books = [
-  {
-    name: `Harry Potter and the Sorcerer's Stone`,
-    genre: 'Fantasy',
-    publisher: 'Scholastic Inc',
-    year: '1997',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SY475_.jpg'
-  },
-  {
-    name: `The Hobbit`,
-    genre: 'Fantasy',
-    publisher: 'Houghton Mifflin',
-    year: '1937',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg'
-  },
-  {
-    name: `The Eye of the World - The Wheel of Time Book 1`,
-    genre: 'Fantasy',
-    publisher: 'Tor Books',
-    year: '1990',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg'
-  },
-  {
-    name: `To Kill A Mockingbird`,
-    genre: 'Southern Gothic',
-    publisher: 'Harper Perennial Modern Classics',
-    year: '1960',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657.jpg'
-  },
-  {
-    name: `The Great Gatsby`,
-    genre: 'Tragedy',
-    publisher: 'Scribner',
-    year: '1925',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490528560l/4671._SY475_.jpg'
-  },
-  {
-    name: 'Dune',
-    genre: 'Sci-Fi',
-    publisher: 'Ace Books',
-    year: '1965',
-    imageUrl:
-      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1555447414l/44767458.jpg'
-  }
-];
-
 // passes |
 app.get('/api/books', (req, res) => {
-  res.json(books);
+  db.any('SELECT* FROM bb_books')
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // passes |
@@ -77,11 +39,11 @@ app.post('/api/books', (req, res) => {
     year: year,
     imageUrl: imageUrl
   };
-  books.push(book);
+  // books.push(book);
 
   res.json({ success: 'book has been successfully added' });
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running...');
 });
---
+++ day-two/book-barn-server/queries/queries.sql
@@ -0,0 +1,46 @@
+-- create users table
+CREATE TABLE bb_users (
+  	user_id SERIAL PRIMARY KEY,
+  	first_name VARCHAR(30),
+  	last_name VARCHAR(30),
+  	user_name TEXT UNIQUE,
+  	user_email TEXT UNIQUE,
+  	user_password VARCHAR(300)
+  )
+
+CREATE TABLE bb_books (
+	book_id SERIAL PRIMARY KEY,
+    name VARCHAR (200),
+    genre VARCHAR (20),
+    publisher VARCHAR (50),
+    year INTEGER,
+    imageUrl TEXT 
+)
+
+INSERT INTO bb_books (name, genre, publisher, year, imageURL)
+VALUES
+('Harry Potter and the Sorcerer"s Stone', 'Fantasy', 'Scholastic Inc', 1997, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SY475_.jpg'),
+('The Hobbit', 'Fantasy', 'Houghton Mifflin', 1937, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg'),
+('The Eye of the World - The Wheel of Time Book 1', 'Fantasy', 'Tor Books', 1990, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665.jpg'),
+('To Kill A Mockingbird', 'Southern Gothic', 'Harper Perennial Modern Classics', 1960,'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657.jpg'),
+('The Great Gatsby', 'Tragedy', 'Scribner', 1925, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490528560l/4671._SY475_.jpg'),
+('Dune', 'Sci-Fi','Ace Books', 1965,'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1555447414l/44767458.jpg')
+
+
+-- Added column to users table
+ALTER TABLE users
+ADD COLUMN user_email TEXT UNIQUE
+
+-- Delete all test users from users table
+DELETE FROM users
+
+UPDATE books
+SET column_value = ''
+WHERE id = ''
+
+-- added column to indicate whether a user is a test user or not
+ALTER TABLE users
+ADD COLUMN test_user BOOLEAN DEFAULT FALSE
+
+-- I updated the column values to true since all users at this point are test users
+UPDATE users SET test_user = true
